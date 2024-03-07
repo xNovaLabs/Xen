465ac4af2cc6a4ee3faa64690e6296e93a899b0a
pyro <pyro>
2024-03-07 14:34:33 -0600

    adding marketplace functionality for future updates

--- src/js/marketplace.js
+++ src/js/marketplace.js
@@ -118,7 +118,8 @@ const gameMethods = ()=> {
         let nums = JSON.stringify(localStorage['installedApps'])
         
 
-        if (nums.includes(gameData.name)) {
+        if (!nums.includes(gameData.name)) {
+
         
         gameListing.classList.add('card');
         gameListing.innerHTML = `
@@ -131,18 +132,76 @@ const gameMethods = ()=> {
             <a href="#">
                 <h5 class="mb-2 items-strech text-2xl font-bold tracking-tight text-gray-900 dark:text-gray">${gameData.name}</h5>
             </a>
+            
             <p class="mb-3 font-normal text-gray flex items-stretch">${gameData.body}</p>
-            <a href="${gameData.link}" class="h-full inline-block items-end items-strech px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
-                Launch 🚀
-            </a>
+            <button onclick="installApp('${gameData.name}');" class="h-full inline-block items-end items-strech px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
+                Download ⬇️
+            </button>
         </div>
         </div>
         </div>
         `
-        gameContainer.appendChild(gameListing);
-    }
+    } else {
         
+        gameListing.classList.add('card');
+        gameListing.innerHTML = `
+        <div class="h-full p-12 flex items-stretch">
+        <div class="max-w-sm bg-gray-200 border border-gray-200 rounded-lg shadow">
+        <a href="#">
+            <img class="rounded-t-lg" src="${gameData.image}" alt="" />
+        </a>
+        <div class="p-5">
+            <a href="#">
+                <h5 class="mb-2 items-strech text-2xl font-bold tracking-tight text-gray-900 dark:text-gray">${gameData.name}</h5>
+            </a>
+            
+            <p class="mb-3 font-normal text-gray flex items-stretch">${gameData.body}</p>
+            <button onclick="uninstallApp('${gameData.name}');" class="h-full inline-block items-end items-strech px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
+                Uninstall ⬆️
+            </button>
+        </div>
+        </div>
+        </div>
+        `
+    }
+        gameContainer.appendChild(gameListing);
+
     })
 }
+
+let installedApps = []
+
+function installApp(gameName) {
+    installedApps = JSON.parse(localStorage.getItem("installedApps"))
+
+    installedApps.push(gameName)
+    console.log(installedApps)
+    let strins = JSON.stringify(installedApps)
+    localStorage.setItem("installedApps", strins)
+    installedApps = []
+    location.reload();
+    return false
+}
+
+
+function uninstallApp(gameName) {
+    installedApps = JSON.parse(localStorage.getItem("installedApps"))
+    installedApps = installedApps.filter(item => item !== String(gameName))
+    console.log(installedApps)
+    let strins = JSON.stringify(installedApps)
+    localStorage.setItem("installedApps", strins)
+    installedApps = []
+    location.reload();
+    return false
+}
+
+window.addEventListener("load", function() {
+    if (localStorage['installedApps'] == null) {
+        this.localStorage.setItem("installedApps", JSON.stringify(installedApps))
+        this.window.location.reload();
+    }
+    
+})
+
 
 gameMethods()
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/*.{html,js}", "./src/js/*.js"],
   theme: {
     extend: {},
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+    }
   },
   plugins: [],
 }
