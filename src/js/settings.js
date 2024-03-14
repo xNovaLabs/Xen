33ebfed38a5debcad5630e13c4514a0bd421ffac
pyro <90619153+builtbypyro@users.noreply.github.com>
2024-03-14 01:00:48 -0500

    Cloaking Presets

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,11 +4,24 @@ const panicSave = document.getElementById("panicButtonSave")
 const panicLink = document.getElementById("panicLink")
 const panic = document.getElementById("panic")
 const favicon = document.getElementById("favicon")
+const dropdownButton = document.getElementById('dropdownDefaultButton');
 
 
 save.addEventListener("click", function() {
     localStorage.setItem("title", title.value)
     localStorage.setItem("favicon", favicon.value)
+    if (dropdownButton.querySelector('span').textContent  == "google") {
+        localStorage.setItem("title", "Google")
+        localStorage.setItem("favicon", "https://www.google.com/favicon.ico")
+    } else if (dropdownButton.querySelector('span').textContent  == "canvas") {
+        localStorage.setItem("title", "Canvas")
+        localStorage.setItem("favicon", "https://gisd.instructure.com/favicon.ico")
+    } else if (dropdownButton.querySelector('span').textContent  == "gmail") {
+        localStorage.setItem("title", "Gmail")
+        localStorage.setItem("favicon", "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico")
+    }
+    
+    
 })
 
 panicSave.addEventListener("click", function() {
